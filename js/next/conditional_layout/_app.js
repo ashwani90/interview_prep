9f67ac56456f55ce29a9961cef9db19fb3c14292
Ashwani <ashwani90125@gmail.com>
2025-09-06 20:02:01 +0530

    Build condition layout

---
+++ js/next/conditional_layout/_app.js
@@ -0,0 +1,13 @@
+// pages/_app.js
+import LayoutWrapper from '../components/LayoutWrapper';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <LayoutWrapper>
+      <Component {...pageProps} />
+    </LayoutWrapper>
+  );
+}
+
+export default MyApp;
