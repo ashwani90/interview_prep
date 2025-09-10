ae238ea8384d35ffdd099ded017ef59ea895ad30
Ashwani <ashwani90125@gmail.com>
2025-09-11 00:13:22 +0530

    analytics app

---
+++ js/next/g-analytics/ga.js
@@ -0,0 +1,36 @@
+// components/GoogleAnalytics.js
+'use client';
+
+import Script from 'next/script';
+import { GA_TRACKING_ID } from '../lib/gtag';
+
+export default function GoogleAnalytics() {
+  if (!GA_TRACKING_ID) {
+    return null;
+  }
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+              transport_type: 'beacon',
+              anonymize_ip: true,
+            });
+          `,
+        }}
+      />
+    </>
+  );
+}
