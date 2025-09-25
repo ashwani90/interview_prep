91a342ab6b9ba9caad2a392fd7f326e813aeb290
Ashwani <ashwani90125@gmail.com>
2025-09-25 23:38:02 +0530

    Animate component in react

---
+++ js/react/advanced-react/animated/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserProfile from './components/UserProfile/UserProfile';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>User Profile</h1>
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
