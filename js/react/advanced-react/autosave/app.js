040849aac6e64125df2b9346953f6a257fcdb2b0
Ashwani <ashwani90125@gmail.com>
2025-09-29 23:45:56 +0530

    Autosave in react

---
+++ js/react/advanced-react/autosave/app.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import AutoSaveForm from './components/AutoSaveForm';
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <AutoSaveForm />
+    </div>
+  );
+};
+
+export default App;
