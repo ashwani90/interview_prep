0701a734494fe3d7b103751f4bba933adad83e04
Ashwani <ashwani90125@gmail.com>
2025-10-21 00:47:56 +0530

    Debounce in react

---
+++ js/react/begin-react/debounce/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Search from "./components/Search";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Debounced Search Example</h1>
+      <Search />
+    </div>
+  );
+}
