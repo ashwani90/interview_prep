56d3fbdfbd7ebc80e69233b2eaef712c438a0d19
Ashwani <ashwani90125@gmail.com>
2025-10-21 00:48:34 +0530

    Dyanmics in react

---
+++ js/react/begin-react/dynamic/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import DynamicForm from "./components/DynamicForm";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Dynamic Form with Validation</h1>
+      <DynamicForm />
+    </div>
+  );
+}
