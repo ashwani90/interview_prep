3665bd1af17fa278af815920279b76d6d3d23f0b
Ashwani <ashwani90125@gmail.com>
2025-10-22 12:26:49 +0530

    Galery in react

---
+++ js/react/begin-react/gallery/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ImageGallery from "./components/ImageGallery";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Image Gallery with Lazy Loading</h1>
+      <ImageGallery />
+    </div>
+  );
+}
