4edd42ec925a51c68ec45b85c745303bfa7722f2
Ashwani <ashwani90125@gmail.com>
2025-11-04 20:06:53 +0530

    Notification system in react

---
+++ js/react/begin-react/notification-system/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import NotificationDemo from "../components/NotificationDemo";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>React Toast Notification Example</h1>
+      <NotificationDemo />
+    </div>
+  );
+}
