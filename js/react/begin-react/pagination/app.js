54dfaefeffef24e7c39d9fd706434aa45cd0987e
Ashwani <ashwani90125@gmail.com>
2025-11-05 22:51:06 +0530

    Build pagination api

---
+++ js/react/begin-react/pagination/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PaginatedList from "./components/PaginatedList";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>React Pagination Component</h1>
+      <PaginatedList />
+    </div>
+  );
+}
