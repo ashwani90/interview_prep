cbac2f2c0431a7f7ef0add53211a5422486b15a3
Ashwani <ashwani90125@gmail.com>
2025-11-07 23:48:44 +0530

    Protected route

---
+++ js/react/begin-react/protected-route/dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useAuth } from "../context/AuthContext";
+
+export default function Dashboard() {
+  const { user, logout } = useAuth();
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome, {user?.name}!</p>
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
+}
