10ad8b9de8b3218581e06ebcc93726f8d4a9587d
Ashwani <ashwani90125@gmail.com>
2025-11-13 20:19:53 +0530

    Bmi in react

---
+++ js/react/inter-react/bmi/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import BMICalculator from "./components/BMICalculator/BMICalculator";
+
+export default function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <BMICalculator />
+    </div>
+  );
+}
