418d96ee33df2123cf8d619f2eddf578093b8f87
Ashwani <ashwani90125@gmail.com>
2025-12-28 23:19:35 +0530

    Redux devtools

---
+++ js/redux/devtools/store.js
@@ -0,0 +1,9 @@
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(
+    applyMiddleware(...middleware)
+    // other store enhancers if any
+  )
+);
