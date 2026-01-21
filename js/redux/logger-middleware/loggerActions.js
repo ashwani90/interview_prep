3da566a09a1f916f726d51a4d5eebf048e169222
Ashwani <ashwani90125@gmail.com>
2026-01-21 23:25:42 +0530

    Logger middleware in redux

---
+++ js/redux/logger-middleware/loggerActions.js
@@ -0,0 +1,11 @@
+export const enableLogging = () => ({
+    type: 'LOGGER/ENABLE'
+  });
+  
+  export const disableLogging = () => ({
+    type: 'LOGGER/DISABLE'
+  });
+  
+  export const toggleLogging = () => ({
+    type: 'LOGGER/TOGGLE'
+  });
