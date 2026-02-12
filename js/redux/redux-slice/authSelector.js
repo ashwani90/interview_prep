0245d8ccd0527b0d30d9629ca478d4aae2fb5dc5
Ashwani <ashwani90125@gmail.com>
2026-02-12 17:07:42 +0530

    Redux slice

---
+++ js/redux/redux-slice/authSelector.js
@@ -0,0 +1,5 @@
+export const selectAuth = (state) => state.auth;
+export const selectCurrentUser = (state) => state.auth.user;
+export const selectAuthToken = (state) => state.auth.token;
+export const selectAuthLoading = (state) => state.auth.isLoading;
+export const selectAuthError = (state) => state.auth.error;
