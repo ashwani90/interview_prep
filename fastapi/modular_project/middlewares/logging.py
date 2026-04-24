a90f75560be0c2f673c80b6de215fec8ab8634e6
Ashwani <ashwani90125@gmail.com>
2026-04-24 14:33:15 +0530

    Adding middlewares

---
+++ fastapi/modular_project/middlewares/logging.py
@@ -0,0 +1,6 @@
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    print(f"Request: {request.method} {request.url}")
+    response = await call_next(request)
+    print(f"Response: {response.status_code}")
+    return response
