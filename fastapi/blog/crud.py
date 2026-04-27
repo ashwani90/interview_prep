51ef07dcdd086ad14af268a2ac6898d25d279b13
Ashwani <ashwani90125@gmail.com>
2026-04-27 21:19:00 +0530

    Fastapi practice

---
+++ fastapi/blog/crud.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import Session
+import models, schemas
+from auth import hash_password
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed = hash_password(user.password)
+    db_user = models.User(name=user.name, email=user.email, password=hashed)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_users(db: Session):
+    return db.query(models.User).all()
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def create_post(db: Session, post: schemas.PostCreate, user_id: int):
+    db_post = models.Post(**post.dict(), user_id=user_id)
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+    return db_post
+
+def get_posts(db: Session):
+    return db.query(models.Post).all()
