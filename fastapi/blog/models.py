13f720c476962b5c140ba5db075c015c962e3b3e
Ashwani <ashwani90125@gmail.com>
2026-04-28 19:32:05 +0530

    Fastapi practice

---
+++ fastapi/blog/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    emails = Column(String, unique=True)
+    password = Column(String)
+    role = Column(String, default="user")
+    posts = relationship("Post", back_populates="owner")
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    content = Column(String)
+    owner_id = Column(Integer, ForeignKey("users.id")) # this adds a field in the table that we can use
+    owner = relationship("User", back_populates="posts")
+
