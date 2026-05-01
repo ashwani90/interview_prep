4321f283807a50e6677e03cce08ed97e691a6f0c
Ashwani <ashwani90125@gmail.com>
2026-05-01 12:09:30 +0530

    Resnet block

---
+++ pytorch-net/vgg/resnet/basic_block.py
@@ -0,0 +1,21 @@
+class BasicBlock(nn.Module):
+    def __init__(self, in_c, out_c, stride=1):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_c)
+        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_c)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_c != out_c:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_c, out_c, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(out_c)
+            )
+        
+        def forward(self, x):
+            out = F.relu(self.bn1(self.conv1(x)))
+            out = self.bn2(self.conv2(out))
+            out += self.shortcut(x)
+            return F.relu(out)
+
