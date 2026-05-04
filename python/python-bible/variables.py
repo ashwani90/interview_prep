5da14182255d0141bd6c8bd993756485c8658ad5
Ashwani <ashwani90125@gmail.com>
2026-05-05 00:02:58 +0530

    Variable change

--- python/python-bible/variables.py
+++ python/python-bible/variables.py
@@ -9,3 +9,34 @@ x = y = [1,2,4]
 x = [1,2,4]
 y = x
 
+# set items inside it must be hashable
+a = {1, 2, 'test'}
+
+# Hashable objects which compare equality must have the same hash value
+
+# Simple conversion
+a = 'hello'
+list(a) # ['h', 'e', 'l', 'l', 'o']
+set(a) # {'h', 'e', 'l', 'o'}
+tuple(a) # ('h', 'e', 'l', 'l', 'o')
+
+# Immutable data types
+# int, long, float, complex
+# str, bytes, tuple, frozenset
+
+# mutable data types
+# bytearray list set dict
+
+names = ['Alice', 'Bob', 'Charlie']
+names.append('David')
+names.insert(1, 'Eve')
+
+names.remove('Charlie')
+
+names.index('David')
+len(names)
+
+names.count('Alice')
+names.reverse() # or names[::-1]
+
+names.pop()
