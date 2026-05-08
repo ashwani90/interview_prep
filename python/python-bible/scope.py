0d20255db8992713b058357c91fbe286522c5613
Ashwani <ashwani90125@gmail.com>
2026-05-09 00:10:16 +0530

    Nonlocal scope usega

--- python/python-bible/scope.py
+++ python/python-bible/scope.py
@@ -10,3 +10,12 @@ def counter():
 
 # So this nonlocal keyword allows us to change the outer scope variable we can use it easily
 # an assignment to global variable inside a local scope will overrider the same name variable
+# global keyword tells program to use global declaration of the code
+
+x = 'hi'
+def change_global():
+    global x
+    x = 'hello'
+    # Changes the global value
+    print(x)
+
