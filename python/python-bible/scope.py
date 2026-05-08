cbc4eba3e4f9419c5f90a4cbb678bd5d1b12c23b
Ashwani <ashwani90125@gmail.com>
2026-05-09 00:08:35 +0530

    Nonlocal scope usega

--- python/python-bible/scope.py
+++ python/python-bible/scope.py
@@ -7,3 +7,6 @@ def counter():
         num += 1
         return num
     return increment
+
+# So this nonlocal keyword allows us to change the outer scope variable we can use it easily
+# an assignment to global variable inside a local scope will overrider the same name variable
