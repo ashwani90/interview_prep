523e7e6adeec2f156cd60f6508f359601d552a54
Ashwani <ashwani90125@gmail.com>
2026-05-11 23:48:42 +0530

    Python info

---
+++ python/python-bible/list.py
@@ -0,0 +1,30 @@
+# various operations on list
+# append an item to list
+l.append(1)
+
+# extend a list with another list
+a.extend(b)
+
+# index get index of first occurence of value
+a.index(7)
+
+# insert(index, value) - insert value just before the specified index
+a.insert(0, 5)
+
+# removes and returns item at the index
+a.pop(2)
+
+# remoe first occurence of the value
+a.remove(8)
+
+# reverse the list
+a.reverse()
+
+# count values in the list
+a.count(7)
+
+# sort the list
+a.sort(reverse=True)
+
+# list can also be sorted using attrgetter and itemgetter
+# 141
