4b888de02a196c87143fa4ddb3cc004a2b2effb2
Ashwani <ashwani90125@gmail.com>
2026-05-18 19:19:18 +0530

    Python add

--- python/python-bible/list_comps.py
+++ python/python-bible/list_comps.py
@@ -1,5 +1,16 @@
 squares = [x*x for x in range(10)]
-
+# list comps are a bit faster
 # More complex
 a = [x for x in range(10) if x % 2 == 0 else None]
+
+# Whitespaces in comprehensions are also allowed
+
+# Conditional list expression 
+[x for x in range(10) if x % 2 == 0]
+# prints even values
+
+# flatten a list
+reduce(lambda x, y: x + y, [[1, 2], [3, 4], [5, 6]])
+
+# list comprehension will allow the best time complexity
 
