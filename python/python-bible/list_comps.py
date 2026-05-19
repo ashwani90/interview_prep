c4b9abb2141db2a7b8706284cc8f8e04a7f55082
Ashwani <ashwani90125@gmail.com>
2026-05-19 18:46:06 +0530

    List ops python

--- python/python-bible/list_comps.py
+++ python/python-bible/list_comps.py
@@ -14,3 +14,23 @@ reduce(lambda x, y: x + y, [[1, 2], [3, 4], [5, 6]])
 
 # list comprehension will allow the best time complexity
 
+# comprehension involving tuples
+
+[x+y for x, y in [(1, 2), (3, 4), (5, 6)]]
+
+# counting occurences using comprehensions
+print(sum(
+    1 for x in range(1000)
+    if x % 2 == 0 and '9' in str(x)
+))
+
+# changing types in alist
+
+items = ["1", "2", "3"]
+numbers = [int(x) for x in items]
+
+# iterate two lists 
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+result = [x + y for x, y in zip(list1, list2)]
+
