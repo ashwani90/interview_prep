c759cd227dd246adbed61565abfb927669dfa59f
Ashwani <ashwani90125@gmail.com>
2026-05-20 18:05:13 +0530

    Linked list with ptyhon

--- python/python-bible/linked_list.py
+++ python/python-bible/linked_list.py
@@ -65,5 +65,93 @@ class LinkedList:
             raise ValueError
             print("Item not found in the list")
     
-    def insert(self, pos, item):
-        
+    def insert(self, position, item):
+        """
+        Insert item at position specified. If position specified is
+        out of bounds, raise IndexError
+        """
+        if position > self.size() - 1 or position < 0:
+            raise IndexError("Index out of bounds.")
+        current = self.head
+        previous = None
+        pos = 0
+        if position == 0:
+            self.add(item)
+        else:
+            new_node = Node(item)
+            while pos < position:
+                pos += 1
+                previous = current
+                current = current.getNext()
+            previous.setNext(new_node)
+            new_node.setNext(current)
+
+    def index(self, item):
+        """
+        Return the index where item is found.
+        If item is not found, return None.
+        """
+        current = self.head
+        pos = 0
+        found = False
+        while current is not None and not found:
+            if current.getData() is item:
+                found = True
+            else:
+                current = current.getNext()
+                pos += 1
+        if found:
+            pass
+        else:
+            pos = None
+        return pos
+
+    def pop(self, position = None):
+        """
+        If no argument is provided, return and remove the item at the head.
+        If position is provided, return and remove the item at that position.
+        If index is out of bounds, raise IndexError
+        """
+        if position > self.size():
+            print('Index out of bounds')
+            raise IndexError
+
+        current = self.head
+        if position is None:
+            ret = current.getData()
+            self.head = current.getNext()
+        else:
+            pos = 0
+            previous = None
+            while pos < position:
+                previous = current
+                current = current.getNext()
+                pos += 1
+            ret = current.getData()
+            previous.setNext(current.getNext())
+            print(ret)
+            return ret
+
+    def append(self, item):
+        """Append item to the end of the list"""
+        current = self.head
+        previous = None
+        pos = 0
+        length = self.size()
+        while pos < length:
+            previous = current
+            current = current.getNext()
+            pos += 1
+        new_node = Node(item)
+        if previous is None:
+            new_node.setNext(current)
+            self.head = new_node
+        else:
+            previous.setNext(new_node)
+
+    def printList(self):
+        """Print the list"""
+        current = self.head
+        while current is not None:
+            print(current.getData())
+            current = current.getNext()
